26f668fccb8e9c815839316a14ba561fd8fd4ed5
Gilmar J. A. Goulart <gilmar.g@outlook.com>
2017-10-24 19:59:21 -0200

    Commit inicial

---
+++ src/main/MainProgram.java
@@ -0,0 +1,37 @@
+package main;
+
+public class MainProgram {
+
+	/*
+	Instruções:
+		Desenvolva uma estratégia gulosa para resolver o problema da mochila binária.
+		O algoritmo deverá ler um arquivo de entrada e gerar um arquivo com a solução encontrada.
+		O arquivo de entrada irá conter os nomes de cada item, lucro,  pesos e capacidade da mochila. Como por exemplo:
+		---------------------------------------------------------------------------------------------------------
+		| ARQUIVO DE ENTRADA
+		---------------------------------------------------------------------------------------------------------
+		Londres Paris Dublin Amsterdam Lisboa Madrid Barcelona Ibiza // Nome dos itens
+		40 25 17 35 38 34 54 48                                                                //Lucro
+		49 35 15 43 38 37 21 37                                                                //Pesos
+		137                                                                                    // Capacidade
+		
+		 
+		---------------------------------------------------------------------------------------------------------
+		| ARQUIVO DE SAIDA
+		---------------------------------------------------------------------------------------------------------
+		0 1 1 0 1 1 0 1                       // 1 indica os itens selecionados
+		127                                   // Soma dos lucros dos itens selecionados
+		107                                   // Peso total da mochila
+		 
+		Crie um resumo extendido (máximo de 3 pags, formato SBC, PDF) explicando o problema da mochila, a estratégia empregada e os resultados obtidos.
+		
+		Apresentação dos resultados obtidos e entrega dia 28 de Novembro.
+	*/
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
