705b66229751873be0101fabacf6e3e7de232778
Gilmar J. A. Goulart <gilmar.g@outlook.com>
2017-11-28 15:57:07 -0200

    Adicionado Lucro/Peso

--- src/classes/Item.java
+++ src/classes/Item.java
@@ -1,10 +1,11 @@
 package classes;
 
-public class Item {
+public class Item implements Comparable<Item> {
 	
 	private String nome;
 	private int lucro;
 	private int peso;
+	private float lucro_peso;
 	
 	public Item() {
 		
@@ -14,6 +15,7 @@ public class Item {
 		setNome(nome);
 		setLucro(lucro);
 		setPeso(peso);
+		setLucro_Peso();
 	}
 	//
 	public String getNome() {
@@ -37,8 +39,36 @@ public class Item {
 		this.peso = peso;
 	}
 	
+	public void setLucro_Peso() {
+		this.lucro_peso = (float) getLucro() / (float) getPeso();
+	}
+	
+	public float getLucro_Peso() {
+		return this.lucro_peso;
+	}
+	
 	@Override
 	public String toString(){
-		return getNome() + ", Lucro: " + getLucro() + ", Peso: " + getPeso();
+		return getNome() + ", Lucro: " + getLucro() + ", Peso: " + getPeso() + ", Lucro/Peso: " + getLucro_Peso();
 	}
+
+	@Override
+	public int compareTo(Item i) {
+		// Decrescente (Invertido)
+		return Float.compare(i.getLucro_Peso(), getLucro_Peso());
+
+		// Decrescente (* -1)
+		//return Float.compare(getLucro_Peso(), i.getLucro_Peso()) * -1;
+		
+		//Decrescente (Manual)
+		/*
+		if (getLucro_Peso() > i.getLucro_Peso()) {
+			return -1;
+		} else if (getLucro_Peso() < i.getLucro_Peso()) {
+			return 1;
+		}
+		return 0;
+		*/
+	}
+
 }
